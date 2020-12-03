9b513b9b582fc0ad3ed64c89a74b79fcd7e1b86d
davidangel <david@davidangel.net>
2020-12-02 22:12:20 -0500

    Remove dotfiles from Code directory.

--- clone.sh
+++ clone.sh
@@ -6,4 +6,3 @@ CODE=$HOME/Code
 
 # Personal
 git clone git@github.com:davidangel/davidangel-vuepress.git $CODE/DA/davidangel-vuepress
-git clone git@github.com:davidangel/dotfiles.git $CODE/DA/dotfiles
